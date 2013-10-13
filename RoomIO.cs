07bcf63c6bc55f2cd718a67fc56b9d34ff201cbf
Owen <owenra@gmail.com>
2013-10-13 14:51:24 -0400

    Addition of RoomerSettings class and IO

--- RoomIO.cs
+++ RoomIO.cs
@@ -1,10 +1,13 @@
-// TODO: need the using for Dictionary<,>
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Roomer
 {
     public static class RoomIO
     {
-        public static bool IsValidRoomsFile(string RoomsFilePath)
+        public static bool IsValidRoomsFile(string RoomsFilePath, RoomerSettings roomerSettings)
         {
             // TODO
             // 1. Check that the file can be accessed and exists.
@@ -13,6 +16,16 @@ namespace Roomer
             // 3. Verify that the file doesn't duplicate any unique IDs (rooms, items, exits, etc).
             // 4. Check that the file has a single starting Room.
             // #. [Whatever else?]
+
+            if (!File.Exists(RoomsFilePath))
+            {
+                return (false);
+            }    
+
+
+
+            // Looks good!
+            return (true);
         }
         
         public static bool LoadRoomsFromFile(Dictionary<string, Room> outRooms, string RoomsFilePath)
@@ -26,5 +39,26 @@ namespace Roomer
             
             return (loadWasSuccessful);
         }
+
+        public static bool LoadRoomerSettings(out RoomerSettings roomerSettings)
+        {
+            roomerSettings = new RoomerSettings();
+
+            if (!File.Exists(RoomerSettings.SettingsPath))
+            {
+                return (false);
+            }
+
+            using (FileStream fs = new FileStream(RoomerSettings.SettingsPath, FileMode.Open))
+            {
+                XmlReader xmlReader = XmlReader.Create(fs);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(RoomerSettings));
+
+                roomerSettings = (RoomerSettings)xmlSerializer.Deserialize(xmlReader);
+            }
+
+            return (true);
+        }
+
     }
 }
---
+++ Test Projects/RoomerSettings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roomer;
+
+namespace RoomerSettingsTest
+{
+    class Program
+    {
+        private Roomer.RoomerSettings roomerSettings;
+
+        private void Go()
+        {
+            bool loadWasSuccessful;
+
+            loadWasSuccessful = RoomIO.LoadRoomerSettings(out roomerSettings);
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.Go();
+        }
+    }
+}
