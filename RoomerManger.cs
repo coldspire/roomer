f1c8fb1dd81e341d3a6d8634f82de6acfb62ca93
Owen <owenra@gmail.com>
2013-11-04 14:24:09 -0500

    RoomerManager now holds rooms, no longer using Rooms class

--- RoomerManger.cs
+++ RoomerManger.cs
@@ -4,8 +4,26 @@ namespace Roomer
 {
     public class RoomerManager
     {
-        private static RoomerSettings roomerSettings;
-        private Rooms rooms;
+        private static RoomerSettings roomerSettings;        
+
+        private Dictionary<string, Room> rooms;
+        public Dictionary<string, Room> Rooms
+        {
+            get { return rooms; }
+            private set { rooms = value; }
+        }        
+        
+        public int NumberOfRooms
+        {
+            get { return Rooms ? Rooms.Count : 0; }
+        }
+        
+        public Room StartingRoom
+        {
+            get { return !roomIdStart.IsNullOrEmpty && Rooms ? Rooms[roomIdStart] : null; }
+        }
+        
+        private string roomIdStart;
 
         public RoomerManager()
         {
