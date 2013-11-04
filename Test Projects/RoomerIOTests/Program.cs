2a074611800b52b0b222a4ded4b9e2920b4caf0b
Owen <owenra@gmail.com>
2013-11-04 14:10:21 -0500

    A test project for fiddling with loading/writing roomer data from/to files

---
+++ Test Projects/RoomerIOTests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roomer;
+
+namespace RoomerIOTests
+{
+    class Program
+    {
+        public RoomerManager roomerManger;
+
+        public void Go()
+        {
+            roomerManger = new RoomerManager();
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.Go();
+        }
+    }
+}
