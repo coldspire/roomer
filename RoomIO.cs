b16acad2f7a71950d402345d84c14648e107d89b
Owen <owenra@gmail.com>
2013-11-04 14:13:12 -0500

    IsValidRoomsFile() XML checks, which are pretty ugly

--- RoomIO.cs
+++ RoomIO.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Xml.Schema;
 
 namespace Roomer
 {
@@ -10,19 +11,30 @@ namespace Roomer
         public static bool IsValidRoomsFile(string RoomsFilePath, RoomerSettings roomerSettings)
         {
             // TODO
-            // 1. Check that the file can be accessed and exists.
-            // 2. Validate the file against the Rooms schemas.
             // 2a. The schemas will check that every room/item/exit/etc has a unique ID.
             // 3. Verify that the file doesn't duplicate any unique IDs (rooms, items, exits, etc).
             // 4. Check that the file has a single starting Room.
             // #. [Whatever else?]
-
+            
+            // 1. Check that the file can be accessed and exists.
             if (!File.Exists(RoomsFilePath))
             {
                 return (false);
-            }    
+            }
+            
+            // 2. Validate the file against the Rooms schemas.
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", Path.Combine(roomerSettings.PathSchemas, roomerSettings.RoomsXsdFilename));
 
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas = schemas;
 
+            XmlReader xmlReader = XmlReader.Create(RoomsFilePath);
+            if (!xmlReader.Read())
+            {
+                return (false);
+            }
 
             // Looks good!
             return (true);
@@ -62,3 +74,4 @@ namespace Roomer
 
     }
 }
+
