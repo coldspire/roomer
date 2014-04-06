cbb8787f86640bc462039a9a029cf8e288eb27f5
Owen <owenra@gmail.com>
2014-04-06 18:12:19 -0400

    RoomerGame.cs stub (working on gameplay loop)

---
+++ RoomerGame.cs
@@ -0,0 +1,24 @@
+
+namespace Roomer
+{
+    class RoomerGame
+    {    
+        public RoomerGame()
+        {
+        }
+        
+        public void Play()
+        {
+            //// Gameplay loop.
+            // 1) Decide what to do with user input, if any exists
+            // 2) Process events based on any PC input from previous loop
+            // 3a) Save/load/stop game if PC requests it
+            // 3b) Start/stop events (sound, output text/graphics, etc)   
+            // 4) Process events based on room
+            // 4a) Start/stop events
+            // 5) Process other events (e.g. timed events)
+            // 6) Update state of room, PC, NPC objects 
+            // 7) Get new input from user
+        }
+    }    
+}
