ec9dd0a104bb6e47a4d3fd3583a4bac401a1fc8c
Owen <owenra@gmail.com>
2014-04-06 18:13:12 -0400

    Added RoomerPlay test project for testing actual gameplay

---
+++ Test Projects/RoomerPlay/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roomer;
+
+namespace RoomerPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoomerGame roomerGame = new RoomerGame();
+        }
+    }
+}
