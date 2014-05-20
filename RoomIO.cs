182ab0f2970379444639d37099fb89c9f7df4c45
coldspire <owenra+git@gmail.com>
2014-05-19 20:30:51 -0400

    RoomIO.cs: cleaned up usings

--- RoomIO.cs
+++ RoomIO.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using System.Xml;
+using System.Linq;
 using System.Xml.Linq;
-using System.Xml.Serialization;
 using System.Xml.Schema;
 
 namespace Roomer
